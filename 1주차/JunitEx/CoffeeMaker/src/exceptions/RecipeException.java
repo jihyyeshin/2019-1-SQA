1595ccc810bd10eed123a8c0698ec234bc867fe8
jihyyeshin <tlsjh082@gmail.com>
2019-03-19 13:26:29 +0900

    example

--- 1주차/JunitEx/CoffeeMaker/src/exceptions/RecipeException.java
+++ 1주차/JunitEx/CoffeeMaker/src/exceptions/RecipeException.java
@@ -3,7 +3,9 @@ package exceptions;
 public class RecipeException extends Exception {
 	
 private static final long serialVersionUID = 1L;
+
 	public RecipeException(String msg) {
 		super(msg);
 	}
+
 }
