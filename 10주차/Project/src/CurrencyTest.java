3755ce0e5cdb1f19e6b7ae92b0f02fce29570c0f
jihyyeshin <tlsjh082@gmail.com>
2019-05-13 00:08:42 +0900

    190513 edit file

--- 10주차/Project/src/CurrencyTest.java
+++ 10주차/Project/src/CurrencyTest.java
@@ -16,10 +16,9 @@ public class CurrencyTest extends TestCase{
 	}
 	public void testToEuros() throws IOException {
 		//expectations		
-		expect(mock.getRate("USD", "EUR")).andReturn(1.0);
-		replay(mock);
+		expect(mock.getRate("USD", "EUR")).andReturn(1.0);//1.0을 return할 것이다.
+		replay(mock);//replay mode
 
-		assertEquals(currencyEUR,currencyUSD.toEuros(mock));
-		verify(mock);
+		assertEquals(currencyEUR,currencyUSD.toEuros(mock));//결과가 예상대로인지 확인한다.
 	}
 }
